8646f49805244a709c644751514f348913d6cad6
AnilVengoti <ar306109@gmail.com>
2024-05-31 20:26:29 +0530

    changes in variables

--- variables.sh
+++ variables.sh
@@ -1,6 +1,9 @@
 #! /bin/bash/
 
-echo "Ramesh: Hello suresh,Good Morning"
-echo "Suresh: Hi Ramesh,Very Good Morining"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: I am good Ramesh, How are you"
+$PERSON1=Ramesh
+$PERSON2=Suresh
+
+echo "$PERSON1: Hello $PERSON2,Good Morning"
+echo "$PERSON2: Hi $PERSON1,Very Good Morining"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good $PERSON1, How are you"
