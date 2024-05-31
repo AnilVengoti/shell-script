56b9f3d7ba9ca3ec6280511fb3ffb353af1313d8
AnilVengoti <ar306109@gmail.com>
2024-05-31 20:02:52 +0530

    shell-script

---
+++ variables.sh
@@ -0,0 +1,6 @@
+#! /bin/bash/
+
+echo "Ramesh: Hello suresh,Good Morning"
+echo "Suresh: Hi Ramesh,Very Good Morining"
+echo "Ramesh: How are you Suresh"
+echo "Suresh: I am good Ramesh, How are you"
